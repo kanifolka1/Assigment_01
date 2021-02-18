8c3f8b5edaebfc9508705a92a2410fd13c76b011
kanifolka1 <45638848+kanifolka1@users.noreply.github.com>
2021-02-17 22:18:14 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,148 @@
+// STEP 1//
+/*let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let MyLibraryFunction
+*/
+//STEP 2//
+/* numeric literal:
+1;
+string literal expression:
+'Hello, Zac';
+Bollean literal expresiion :
+true
+null literal expression :
+null;
+*/
+//STEP 3//
+/*complex expression /variable
+var newName ='(firstName + lastName)';
+*/
+//STEP 4//
+/* var firstName; 
+var lastName;
+var  address; 
+var city;
+var state; 
+var zipCode;
+var yourAge;
+var refferalSource;
+var mayWeContactYou;
+*/
+//STEP 5//
+/*
+var city ='San Diego';
+var state = 'CA'; 
+var mayWeContactYou = true;
+*/
+//STEP 6//
+/*
+var position = 12+' module ';
+window.console.log(position);
+*/
+//STEP 7//
+/*
+var newAddress = false + ' (house)';
+window.console.log(newAddress);
+
+var yearsLiving = 10 + true;
+window.console.log (yearsLiving);
+*/
+//STEP 8//
+/*
+let someString = 'Who once said : "Only two things are infinite, the  universe and human stupidity, and I\'m not sure about the former." ';
+window.console.log (someString);
+*/
+//STEP 9//
+/*
+ var enable= null;
+ window.console.log(enable);
+
+ var y;
+ window.console.log (y);
+*/
+//STEP 10//
+/*
+var some = "Hi";
+window.console.log(typeof(some));
+
+var num =20;
+window.console.log(typeof(num));
+
+var once =true;
+window.console.log(typeof(once));
+
+var x=null;
+window.console.log(typeof(x));
+
+var make;
+window.console.log(typeof(make));
+*/
+//STEP 11//
+/*
+alert("Hello"+" Mariia Liakh,"+ " welcome to the JavaScript class!");
+*/
+//STEP 12//
+/*
+var name = 'Mariia Liakh';
+alert("Hello" + " "+ name+"," + " welcome to the JavaScript class!");
+*/
+//STEP 13//
+/*
+var course = 'JavaScript';
+alert("Hello"+" Mariia Liakh," + " welcome to the "+ course +" class!");
+*/
+//STEP 14//
+/*
+alert("Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!");
+*/
+//STEP 15//
+/*
+var name = prompt( "What is your name?");
+document.write(name);
+*/
+//STEP 16//
+/*
+var course =prompt ("what class are you taking?");
+document.write(course);
+*/
+//STEP 17//
+/*
+var x = 10;
+var y = 20;
+console.log (x+y);
+*/
+//STEP 18//
+/*
+var x = 20;
+x = 20 + x;
+console.log(x);
+*/
+//STEP 19//
+/*
+var x = 20;
+x = 20 * 5;
+console.log (x);
+*/
+//STEP 20//
+/*
+var x = 20%3;
+ y=1;
+ var y=x;
+window.console.log (y);
+*/
+//STEP 21//
+/*
+var x = 10;
+var y = 20;
+console.log(x<y);
+*/
+
+//STEP 22//
+/*
+var area = a + b;
+var a = 5 ;
+var b = 10 ;
+console.log( b<a);
+*/
